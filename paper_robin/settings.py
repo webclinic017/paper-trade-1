57110960f8e53e5bbbf222fa4812894061a8b9a4
Long <lin.lo@husky.neu.edu>
2020-01-19 17:33:01 -0500

    add api app

--- paper_robin/settings.py
+++ paper_robin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'paper_robin.apps.api',
 ]
 
 MIDDLEWARE = [
