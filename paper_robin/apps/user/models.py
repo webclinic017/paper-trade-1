06fdf14f04a4f8af41db5dcc03673f146375af29
Long <lin.lo@husky.neu.edu>
2020-01-20 16:00:39 -0500

    user profile model

--- paper_robin/apps/user/models.py
+++ paper_robin/apps/user/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     custom_field = 5
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
