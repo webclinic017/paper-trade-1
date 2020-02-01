148bb56f812014494559a07c154e02e9732e01c7
lin.lo@husky.neu.edu <lin.lo@husky.neu.edu>
2020-02-01 17:52:32 -0500

    remove useless scripts

--- pr_down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Stopping Paper Robin.
-docker-compose down
-& docker container stop $(docker container ls -aq)
-& yes | docker rmi $(docker images -f "dangling=true" -q) 
-& yes | docker volume prune -f
--- pr_up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo Starting Paper Robin.
-exec docker build -t paper_robin .
-exec docker run -it -a STDOUT -p 8000:8000 paper_robin
