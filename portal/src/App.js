b576e434520ecd3cc7f8578c1c9af7e1eafeed9d
lin.lo@husky.neu.edu <lin.lo@husky.neu.edu>
2020-02-20 18:39:02 -0500

    docker optimization :facepalm

--- portal/src/App.js
+++ portal/src/App.js
@@ -7,7 +7,7 @@ const App = () => (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadsawsssdsad
+          Edit <code>src/App.js</code> and save to reloadsawsssdsads
         </p>
         <a
           className="App-link"
