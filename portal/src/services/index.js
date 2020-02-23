5504670b42423b6ae2cf35aed180ee7804afe2ad
Long <lin.lo@husky.neu.edu>
2020-02-23 01:23:42 -0500

    default remove get from default options

--- portal/src/services/index.js
+++ portal/src/services/index.js
@@ -15,7 +15,6 @@ export {
 const axiosClient = (() => {
     const defaultOptions = {
         baseURL: 'http://127.0.0.1:8000/api/v1',
-        method: 'get',
         headers: {
             'Content-Type': 'application/json',
         },
