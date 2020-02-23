1d6ef876c2f6c942e0e3e73488fa00d284495988
Long <lin.lo@husky.neu.edu>
2020-02-23 01:27:05 -0500

    use params instead of adding in url

--- portal/src/services/stockService.js
+++ portal/src/services/stockService.js
@@ -1,5 +1,7 @@
 import { axios } from '../services';
 
 export const loadDailyStockData = (symbol, date) => 
-    axios.get(`/daily_stock_data/?symbol=${symbol}&date=${date}`);
+    axios.get(`/daily_stock_data/`, {
+        params: { symbol, date }
+    });
 
